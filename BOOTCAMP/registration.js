76e259a1afa9037cee29f07d53d1b25ff2e7c1a6
dhane sah <dhanesah104@gmail.com>
2021-12-24 09:36:51 +0545

    java script to show information

--- BOOTCAMP/registration.js
+++ BOOTCAMP/registration.js
@@ -4,8 +4,28 @@ console.log("this is registration page");
 const registrationform = document.getElementById("registrationform");
 
 console.log(registrationform);
+function getSelected(selecet) {
+let selectedvalues =[]
+for(OptionIndex in selecet) {
+    if ( select.options[OptionIndex].selected) {
+        selectedvalues.push(select.options[OptionIndex].value);
+  }
+}
+ return selectedvalues;
+}
 
 registrationform.onsubmit = function (event) {
     event.preventDefault();
     console.log("i am triggered when submit is preessed");
+    let name = document.getElementById("name").value;
+    let intrest = getSelected(document.getElementById("interest"));
+    let dob = document.getElementById("dob").value;
+    let darkMode = document.getElementById("darkMode").checked;
+    const data ={
+       name: name,
+       interest: interests,
+       dob: dob,
+        darkmode :darkMode,
+    };
+    console.log(data);
 };
